8b20235bdb955d08421d0509cc0be20ebfd1baf8
Maryclare Griffin <maryclaregriffin@gmail.com>
2017-03-21 21:45:18 -0400

    Added data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Tumor data
+#'
+#' A dataset containing gene expression data for 43 tumors.
+#'
+#' @format A list of two items:
+#' \describe{
+#'   \item{Y}{Matrix of 356 continuous gene expression measurements for 43 tumors.}
+#'   \item{class}{Vector giving tumor type for each tumor represented in \code{Y}}
+#'   ...
+#' }
+#' @source \url{https://github.com/julierennes/denoiseR}
+"tumors"
